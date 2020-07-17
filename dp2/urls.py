4950b22d34bca4dee60771b5d23cab51e8f8839e
abhilash <abhidenim10@gmail.com>
2020-07-17 21:17:48 +0530

    templates added

--- dp2/urls.py
+++ dp2/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name="index"),
     path('home',views.home,name="home"),
+    path('htmld',views.htmld,name="htmld"),
 ]
--- dp2/views.py
+++ dp2/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("hello world")
 def home(request):
     return HttpResponse("<h1> welcome to  homepage <h1>")
+def htmld(request):
+    return render(request,"sample1.html")
